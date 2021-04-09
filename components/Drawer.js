7d7c05d5c834f2bb6a5e9dd267e032a14acabfd9
Ugo Palatucci <ugo.palatucci@skillbill.it>
2021-04-09 12:40:19 +0200

    swipe drawer

--- components/Drawer.js
+++ components/Drawer.js
@@ -14,7 +14,6 @@ import {WithLocalSvg} from 'react-native-svg';
 import X from '../assets/x.svg';
 import {connect} from 'react-redux';
 import {LOGOUT} from '../store/mutations';
-import {Swipeable} from 'react-native-gesture-handler';
 import Text from './ui/Text';
 
 const Drawer = ({isLogged, logout}) => {
@@ -29,69 +28,62 @@ const Drawer = ({isLogged, logout}) => {
   };
   return (
     <SafeAreaView style={styles.container}>
-      <Swipeable
-        style={styles.container}
-        containerStyle={styles.container}
-        childrenContainerStyle={styles.container}
-        onSwipeableLeftOpen={() => console.log('Ciao')}
-        onSwipeLeftOpen={() => Actions.drawerClose()}>
-        <View>
-          <TouchableHighlight
-            style={styles.xTouchWidth}
-            onPress={() => Actions.pop()}>
-            <WithLocalSvg style={styles.x} width={20} height={20} asset={X} />
-          </TouchableHighlight>
-        </View>
-        <ScrollView style={styles.scrollView}>
-          <TouchableHighlight onPress={() => Actions.home()}>
-            <Text style={styles.text}>Home</Text>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={() => Actions.buddismo()}>
-            <Text style={styles.text}>Buddismo</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={() =>
-              Actions.posts({
-                title: 'In primo piano',
-                uri: 'news',
-                entrypoint: SGI_ENTRYPOINT,
-              })
-            }>
-            <Text style={styles.text}>In primo piano</Text>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={() => Actions.magazines()}>
-            <Text style={styles.text}>Le riviste</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={() =>
-              Linking.openURL('https://servizi.sgi-italia.org/abbonamenti/')
-            }>
-            <Text style={styles.text}>
-              {Platform.select({
-                ios: 'Pubblicazioni',
-                android: 'Spazio Abbonamenti',
-              })}
-            </Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={() =>
-              Linking.openURL('https://servizi.sgi-italia.org/aderenti/')
-            }>
-            <Text style={styles.text}>Spazio Aderenti</Text>
-          </TouchableHighlight>
-          <TouchableHighlight onPress={loginOrOut}>
-            <Text style={[styles.text, styles.lastText]}>
-              {isLogged ? 'Logout' : 'Login'}
-            </Text>
-          </TouchableHighlight>
-          <Text style={styles.version}>
+      <View>
+        <TouchableHighlight
+          style={styles.xTouchWidth}
+          onPress={() => Actions.pop()}>
+          <WithLocalSvg style={styles.x} width={20} height={20} asset={X} />
+        </TouchableHighlight>
+      </View>
+      <ScrollView style={styles.scrollView}>
+        <TouchableHighlight onPress={() => Actions.home()}>
+          <Text style={styles.text}>Home</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => Actions.buddismo()}>
+          <Text style={styles.text}>Buddismo</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() =>
+            Actions.posts({
+              title: 'In primo piano',
+              uri: 'news',
+              entrypoint: SGI_ENTRYPOINT,
+            })
+          }>
+          <Text style={styles.text}>In primo piano</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={() => Actions.magazines()}>
+          <Text style={styles.text}>Le riviste</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() =>
+            Linking.openURL('https://servizi.sgi-italia.org/abbonamenti/')
+          }>
+          <Text style={styles.text}>
             {Platform.select({
-              ios: '2.0.8 28',
-              android: '2.0.1 201',
+              ios: 'Pubblicazioni',
+              android: 'Spazio Abbonamenti',
             })}
           </Text>
-        </ScrollView>
-      </Swipeable>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={() =>
+            Linking.openURL('https://servizi.sgi-italia.org/aderenti/')
+          }>
+          <Text style={styles.text}>Spazio Aderenti</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={loginOrOut}>
+          <Text style={[styles.text, styles.lastText]}>
+            {isLogged ? 'Logout' : 'Login'}
+          </Text>
+        </TouchableHighlight>
+        <Text style={styles.version}>
+          {Platform.select({
+            ios: '2.0.8 28',
+            android: '2.0.1 201',
+          })}
+        </Text>
+      </ScrollView>
     </SafeAreaView>
   );
 };
